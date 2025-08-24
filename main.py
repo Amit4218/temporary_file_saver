801d4007c8d8d6a9d190d809ae35c1e515dd8082
Amit4218 <amitbhagat621@gmail.com>
2025-08-24 22:17:56 +0530

    feat: added routes and file checks

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request, send_from_directory, abort
 from werkzeug.utils import secure_filename
 from db.db import create_table, save_file_path
+from clean_up_files import delete_files
 from dotenv import load_dotenv
-import subprocess
+import threading
 import uuid
 import os
 
 app = Flask(__name__)
-ROOT_FOLDER = "temp"  # root directory for the folders and files
 load_dotenv()
+ROOT_FOLDER = "temp"  # root directory for the folders and files
+HOST_URL = os.getenv("HOST_BASE_URL")
+MAX_FILE_SIZE = 30 * 1024 * 1024
 
 
 def func():
@@ -23,9 +26,27 @@ def func():
         return f"<h1>{e}</h1>"
 
 
+def check_file_size(file):
+
+    file.stream.seek(0, 2)
+    size = file.stream.tell()
+
+    file.stream.seek(0)
+
+    if size > MAX_FILE_SIZE:
+        return True
+
+    return False
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/api")
+def api():
+    return render_template("api.html", domain_name=HOST_URL)
 
 
 @app.route("/upload", methods=["POST"])
@@ -43,19 +64,26 @@ def upload():
             if file.filename == "":
                 raise Exception("No file was found")
 
+            file_size = check_file_size(file)
+
+            if file_size:
+                raise Exception("File exceeds 30 MB limit")
+
             if file:
                 folder_path, folder_id = func()
                 file_name = secure_filename(file.filename)
                 file_path = os.path.join(folder_path, file_name)
                 file.save(file_path)
 
-                file_url = f"{os.getenv('HOST_BASE_URL')}/file/{folder_id}/{file_name}"
+                file_url = f"{HOST_URL}/file/{folder_id}/{file_name}"
 
-                save_path_to_db = save_file_path(filename=file_name, path=folder_path)
+                path = f"{folder_path}/{file_name}"
 
-                html = f"<h3><a href={file_url} target='_blank'>{file_url}</a></h3>"
+                save_file_path(filename=file_name, path=path)
 
-                return render_template("preview.html", file_url=file_url)
+                return render_template(
+                    "preview.html", file_url=file_url, file_name=file_name
+                )
 
     except Exception as err:
         return render_template("error.html", error=err)
@@ -77,10 +105,53 @@ def get_file(folder, filename):
         return render_template("error.html", error=err)
 
 
+@app.route("/api/v1/upload", methods=["POST"])
+def api_upload():
+    try:
+
+        if request.method == "POST":
+
+            if "file" not in request.files:
+                return "File not found", 404
+
+            file = request.files["file"]
+
+            file_size = check_file_size(file)
+
+            if file_size:
+                return "File exceeds 30 MB limit"
+
+            if file:
+                folder_path, folder_id = func()
+                file_name = secure_filename(file.filename)
+                file_path = os.path.join(folder_path, file_name)
+                file.save(file_path)
+
+                file_url = f"{HOST_URL}/file/{folder_id}/{file_name}"
+
+                path = f"{folder_path}/{file_name}"
+
+                save_file_path(filename=file_name, path=path)
+
+                res_structure = {
+                    "status": "Success",
+                    "File_url": file_url,
+                }
+
+                return res_structure, 200
+
+    except Exception as e:
+        print(e)
+
+
 def main():
 
     create_table()
-    subprocess.run(f"clean_up_files.py && {app.run(port=3000)}", check=True)
+
+    clean_up = threading.Thread(target=delete_files, daemon=True)
+    clean_up.start()
+
+    app.run(port=3000)
 
 
 if __name__ == "__main__":
